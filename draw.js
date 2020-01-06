972d52be447cdcde1b56b8c72e90f622bb8d8ee5
atmarc <marquitus99@gmail.com>
2020-01-06 18:45:50 +0100

    millores amb els options

--- draw.js
+++ draw.js
@@ -1,7 +1,7 @@
 function drawTool (canvas) {
     this.setup = (canvas) => {
         this.canv = document.getElementById(canvas);
-        this.ctx = this.canv.getContext("2d");
+        this.ctx = this.canv.getContext('2d');
         this.width = this.canv.width;
         this.height = this.canv.height;
         this.xOffset = 0;
@@ -12,26 +12,35 @@ function drawTool (canvas) {
         this.setup(canvas);
     }
 
-    this.dot = (x, y, color='black') => {
-        this.ctx.fillStyle = color;
+    this.dot = (x, y, options={color:'red'}) => {
+        this.ctx.fillStyle = options.color;
         this.ctx.fillRect(x, y, 1, 1);
     } 
     
-    this.rectangle = (x, y, w, h, color='black', center=true, stroke=false, width=1, sc="black") => {
-        this.ctx.fillStyle = color;1
-        if (center)
-            this.ctx.fillRect(x - w/2, y - h/2, w, h);
-        else
-            this.ctx.fillRect(x, y, w, h);
-        if (stroke) {
-            console.log('stroke');
-            this.ctx.beginPath();
-            this.ctx.strokeStyle = sc;
-            this.ctx.lineWidth = w;
-            this.ctx.stroke();
-            this.ctx.closePath();        
+    this.rectangle = (x, y, w, h, options) => {
+
+        var opt = initOptions(options)
+
+        this.ctx.fillStyle = opt.color;
+        var posX, posY;
+        if (opt.center) {
+            posX = x - w/2;
+            posY = y - h/2;
+        }
+        else {
+            posX = x;
+            posY = y;
         }
 
+        if (opt.fill) this.ctx.fillRect(posX, posY, w, h);
+
+        if (opt.stroke) {
+            this.ctx.beginPath();
+            this.ctx.strokeStyle = opt.sColor;
+            this.ctx.lineWidth = opt.sWidth;
+            this.ctx.strokeRect(posX, posY, w, h);
+            this.ctx.closePath();        
+        }
     }
 
     this.translate = (x, y) => {
@@ -40,37 +49,44 @@ function drawTool (canvas) {
         this.yOffset += y;
     }
 
-    this.circle = (x, y, r, color = "black", fill=true, w=1) => {
+    this.circle = (x, y, r, options) => {
+
+        var opt = initOptions(options);
+
         this.ctx.beginPath();
-        this.ctx.fillStyle = color;
+        this.ctx.fillStyle = opt.color;
         this.ctx.arc(x, y, r, 0, 2 * Math.PI);
-        if (fill) {
+        if (opt.fill) {
             this.ctx.fill();
         }
-        else {  
-            this.ctx.strokeStyle = color;
-            this.ctx.lineWidth = w;
+        if (opt.stroke) {  
+            this.ctx.strokeStyle = opt.sColor;
+            this.ctx.lineWidth = opt.sWidth;
             this.ctx.stroke();
         }
         this.ctx.closePath();        
     }
 
-    this.line = (x1, y1, x2, y2, color = "black", width=1) => {
+    this.line = (x1, y1, x2, y2, options) => {
+        var opt = options;
+        if (opt.color === undefined) opt.color = 'black'; 
+        if (opt.width === undefined) opt.width = 1; 
+
         this.ctx.beginPath(); 
         this.ctx.moveTo(x1, y1);
         this.ctx.lineTo(x2, y2);
-        this.ctx.lineWidth = width;
-        this.ctx.strokeStyle = color; 
+        this.ctx.lineWidth = opt.width;
+        this.ctx.strokeStyle = opt.color; 
         this.ctx.stroke(); 
         this.ctx.closePath();        
     }
 
-    this.clearRect = (x1, y1, x2, y2, color="white") => {
+    this.clearRect = (x1, y1, x2, y2, color='white') => {
         this.ctx.fillStyle = color;
         this.ctx.clearRect(x1, y1, x2, y2);
     } 
     
-    this.clearAll = (color="white") => {
+    this.clearAll = (color='white') => {
         this.ctx.fillStyle = color;        
         this.ctx.clearRect(0 - this.xOffset, 0 - this.yOffset, d.width, d.height);
     }
@@ -80,6 +96,17 @@ function drawTool (canvas) {
     }
 
     this.rotate = (angle) => {
-        this.ctx.rotate(angle)
+        this.ctx.rotate(angle);
     }
+}
+
+function initOptions (options) {
+    if (options === undefined) var options = {};
+    if (options.color === undefined) options.color = 'black';
+    if (options.fill === undefined) options.fill = true;
+    if (options.center === undefined) options.center = true;
+    if (options.stroke === undefined) options.stroke = false;
+    if (options.sWidth === undefined) options.sWidth = 1;
+    if (options.sColor === undefined) options.sColor = 'black';            
+    return options;
 }
